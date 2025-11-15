0af11ed2f0ead900a295b840b23d79c28a254093
John-O <johnoladosu@outlook.com>
2025-11-15 22:24:46 +0000

    `Added new routes and views for book search and addition, updated existing routes and views for navigation and styling`

--- routes/books.js
+++ routes/books.js
@@ -7,8 +7,17 @@ router.get('/search',function(req, res, next){
 });
 
 router.get('/search-result', function (req, res, next) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+    // Search in the database for books matching the keyword (partial match)
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?"; // Use LIKE for partial matching
+    // Use % wildcards to match any characters before and after the search keyword
+    let searchTerm = "%" + req.query.keyword + "%";
+    // Execute sql query with the search term
+    db.query(sqlquery, [searchTerm], (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.render("list.ejs", {availableBooks:result})
+    });
 });
 
 router.get('/list', function(req, res, next) {
@@ -18,9 +27,34 @@ router.get('/list', function(req, res, next) {
             if (err) {
                 next(err)
             }
-            res.send(result)
+            res.render("list.ejs", {availableBooks:result})
          });
 });
+
+router.get('/bargainbooks', function(req, res, next) {
+    let sqlquery = "SELECT name, price FROM books WHERE price < 20"; // query database for books under £20
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.render("list.ejs", {availableBooks:result})
+    });
+});
+
+router.post('/bookadded', function (req, res, next) {
+    // saving data in database
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+    // execute sql query
+    let newrecord = [req.body.name, req.body.price]
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        else
+            res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price)
+    })
+}) 
 
 // Export the router object so index.js can access it
 module.exports = router
